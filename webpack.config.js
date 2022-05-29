546cee71bd2b3259ae8e5468118b42b7c9d12efd
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-05-29 20:42:00 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const { resolve } = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
