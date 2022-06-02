e86bb3d0e9e574fc15d1331cbd9e4e06ecb95587
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-06-02 19:29:01 +0300

    add /__mocks__

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,36 @@
-import { test } from "./index";
+import { saveList } from "./index";
 
-describe("test", () => {
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+describe("saveList", () => {
   it("is a function", () => {
-    expect(typeof test).toBe("function");
+    expect(saveList).toBeInstanceOf(Function);
+  });
+  it('save ["moscow", "london", "rome", "new york", "boston"] in localstorage', () => {
+    saveList("list", ["moscow", "london", "rome", "new york", "boston"]);
+    const items = localStorage.getItem("rome");
+    expect(items).toEqual("rome");
   });
 });
